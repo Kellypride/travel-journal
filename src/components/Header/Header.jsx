bc3d23768691df1a2badb0e781a022c1725ff8a5
kellypride <ngehkellypride181@gmail.com>
2024-03-14 23:19:39 +0100

    Set up project structure and files.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Header () {
+    return (
+        <h1 className="bg-blue-500">Welcome to my travel journal </h1>
+    )
+}
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Main () {
+    return (
+        <div>
+            <p className=" bg-blue-green-200 ">My main section of  my tarvel journal</p>
+        </div>
+    )
+}
