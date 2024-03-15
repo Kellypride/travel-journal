af33c7a25fae91be31426a0d85a0ff45cf672c3f
kellypride <ngehkellypride181@gmail.com>
2024-03-15 04:51:19 +0100

    Implemented header and main section of my travel journal.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-
+import data from "./data"
 function App() {
+  const cards = data.map (item =>{
+    return (
+      <Main
+       key = {item.id} 
+       item = {item}
+      />
+
+    )
+  })
   return (
-    <div className="App">
+    <div>
       <Header />
-      <Main />
+      <div className="flex flex-col gap-5 py-10 px-5">
+        {cards}
+      </div>
     </div>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+// import image1 from "../../assets/australia.png"
 
-export default function Main () {
+// import Items from "../../data";
+
+export default function Main (props) {
     return (
-        <div>
-            <p className=" bg-blue-green-200 ">My main section of  my tarvel journal</p>
-        </div>
+        <main className="py-10 px-5 flex gap-5 justify-center items-center font-montserrat text-black box-border">
+            <img src={require('../../assets/'+props.item.img)} alt="japan img" />
+            <div className="flex flex-col gap-2">
+                <div className="flex gap-4 items-center justify-center text-sm font-light">
+                    <span className="flex justify-center items-center gap-1">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 16 16"><path fill="red" d="M8 0C5.2 0 3 2.2 3 5s4 11 5 11s5-8.2 5-11s-2.2-5-5-5m0 8C6.3 8 5 6.7 5 5s1.3-3 3-3s3 1.3 3 3s-1.3 3-3 3"></path></svg>
+                       <p className="text-lg font-meduim ">{props.item.location} Janpan</p>
+
+                    </span>
+                    <button className="text-gray-400 underline decoration-1 underline-offset-4">{props.link} view google map</button>
+                </div>
+                <h2 className="font-bold text-3xl"> {props.item.city} Mount Fuji</h2>
+                <h3 className="font-medium text-sm py-3">222</h3>
+                <p className=" font-light text-md tracking-tight ">{props.item.description} mount fuji is the tallest</p>
+
+            </div>
+        </main>
     )
 }
